cf0bc3a6e122dededbe0fa6a2dc22fee88cdb095
George Yu <dumpling_yu@163.com>
2018-04-17 20:35:54 +0800

    Optimize counter animation

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,5 @@
 const LAP_LENGTH = 50; // in meters
-const METER_UPDATE_SPAN = 1000; // in milliseconds
+const METER_UPDATE_SPAN = 950; // in milliseconds
 const LEADER_COLS_WIDTH = [0, 0.08, 0.6, 0.75];
 const LEADER_LINE_HEIGHT = 46 * 1.5;
 
