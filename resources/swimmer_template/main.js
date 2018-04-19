d839a65aebaa580373ddb0116fd5880109e6ef50
George Yu <dumpling_yu@163.com>
2018-04-19 16:24:04 +0800

    Optimize code

--- resources/swimmer_template/main.js
+++ resources/swimmer_template/main.js
@@ -1,34 +1,28 @@
-previous_lap_count=null;
-current_lap_count=null;
-current_meters_count=null;
-previous_meters_count=null;
-function increment(type,element,updateCount) {
-    if (type=='Lap'){
-        current = previous_lap_count;
-    }
-    else if (type=='Meters'){
-        current = previous_meters_count;
-    }
-    element.prop('counter', current).animate({counter: current + updateCount}, {
-        duration: 2000,
+let currentLaps = 0;
+
+function set_stat(element, to, span=2000) {
+    let current = parseInt(element.text());
+    if (current === to) return;
+    element.prop('counter', current).animate({counter: to}, {
+        duration: span,
         easing: 'swing',
         step: now => element.text(Math.ceil(now))
     });
 }
 
-(function update() {
-    $(document).ready(() => {
-        $.getJSON(SWIMMER_DATA_FILE, raw => {
-            current_lap_count=raw.laps;
-            current_meters_count=current_lap_count*LAP_LENGTH;
-            change=current_lap_count-previous_lap_count;
-            $('#name').text(raw.name);
-            increment('Lap',$('#laps'),change);
-            increment('Meters',$('#meters'),change * LAP_LENGTH);
-            document.title = `${raw.name} | Swim For Love`;
-            previous_lap_count=current_lap_count;
-            previous_meters_count=current_meters_count;
-        });
+$(document).ready(() => {
+    $.ajaxSetup({cache: false}); // no cache
+    // Initialize
+    $.getJSON(SWIMMER_DATA_FILE, raw => {
+        $('#name').text(raw.name);
+        set_stat($('#laps'), raw.laps);
+        set_stat($('#meters'), raw.laps * LAP_LENGTH);
+        document.title = `${raw.name} | Swim For Love`;
     });
-    setTimeout(update, 500);
-})();
+    setInterval(() => {
+        $.getJSON(SWIMMER_DATA_FILE, raw => {
+            set_stat($('#laps'), raw.laps);
+            set_stat($('#meters'), raw.laps * LAP_LENGTH);
+        });
+    }, 2000);
+});
