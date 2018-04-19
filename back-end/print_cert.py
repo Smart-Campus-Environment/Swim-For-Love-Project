f274fabf993ca4a96448d6c8a64ee8ab35854917
He Huang <peter@peterhuang.cn>
2018-04-19 16:37:32 +0800

    Update

---
+++ back-end/print_cert.py
@@ -0,0 +1,44 @@
+import os
+import time
+
+wait_time = 1
+base_url= 'http://localhost/Swim-For-Love-Project'
+
+
+def browser(url):
+	os.system("""osascript -e 'tell application "Safari" to set the URL of the front document to " """+url+""" "'""")
+
+def Demo():
+	while True:
+		UID=input('UID:').upper()
+		browser(base_url+'/swimmers/'+UID+'/cert.html')
+		activate_Safari()
+		time.sleep(wait_time)
+		os.system("""osascript -e 'tell application "System Events" to key code 36'""")
+		time.sleep(wait_time)
+		activate_Terminal()
+
+
+
+
+
+def activate_Safari():
+	os.system("""
+	osascript -e '
+	tell application "Safari"
+		activate
+	end tell
+	'
+		""")
+
+def activate_Terminal():
+	os.system("""
+	osascript -e '
+	tell application "Terminal"
+		activate
+	end tell
+	'
+		""")
+
+
+Demo()
