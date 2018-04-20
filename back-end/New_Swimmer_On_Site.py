929c41496831d7327662b3e546bfc11105b04354
He Huang <peter@peterhuang.cn>
2018-04-20 18:19:25 +0800

    Bug Fix

--- back-end/New_Swimmer_On_Site.py
+++ back-end/New_Swimmer_On_Site.py
@@ -3,16 +3,24 @@ import json
 from config import *
 
 def Add_Photo():
-	try:
-		photo_name=os.listdir(TEMP_PHOTO_PATH)[0]
+	file_list=[]
+	for files in os.listdir(TEMP_PHOTO_PATH):
+		if files.endswith('.jpg'):
+			file_list.append(files)
+	if len(file_list) ==1:
+		photo_name=file_list[0]
 		os.rename(TEMP_PHOTO_PATH+photo_name,REGISTER_PATH+uid+'.jpg')
-	except:
+	elif len(file_list)==0:
 		decision=input('Photo does not exist in File, proceed? (Y/N):').lower()
 		if decision=='y' or decision=='yes':
 			pass
 		else:
 			input('Please Press Enter after taking photo')
 			Add_Photo()
+	elif len(file_list)>1:
+		print('There are more than one file in Temp_File Folder')
+		input('Please Press Enter after leaving one picture.')
+		Add_Photo()
 
 name=input('Name:')
 uid=input('UID:')
