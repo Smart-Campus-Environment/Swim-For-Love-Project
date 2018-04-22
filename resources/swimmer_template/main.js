e0272a4ac0367e88d45ec25701b49bf1306e8bda
He Huang <peter@peterhuang.cn>
2018-04-22 13:09:01 +0800

    Color Change for Swimmer

--- resources/swimmer_template/main.js
+++ resources/swimmer_template/main.js
@@ -1,5 +1,4 @@
 let currentLaps = 0;
-
 function set_stat(element, to, span=2000) {
     let current = parseInt(element.text());
     if (current === to) return;
@@ -10,10 +9,26 @@ function set_stat(element, to, span=2000) {
     });
 }
 
+function BG_Update(laps){
+    if (laps<BRONZE_MEDAL_LAP_COUNT){
+        document.getElementById("bg").style.background = NO_MEDAL_BG;
+    }
+    else if (laps>=BRONZE_MEDAL_LAP_COUNT && laps<SILVER_MEDAL_LAP_COUNT){
+        document.getElementById("bg").style.background = BRONZE_MEDAL_BG;
+    }
+    else if (laps>=SILVER_MEDAL_LAP_COUNT && laps<GOLD_MEDAL_LAP_COUNT){
+        document.getElementById("bg").style.background = SILVER_MEDAL_BG; 
+    }
+    else if (laps>=GOLD_MEDAL_LAP_COUNT){
+        document.getElementById("bg").style.background = GOLD_MEDAL_BG; 
+    }
+
+}
 $(document).ready(() => {
     $.ajaxSetup({cache: false}); // no cache
     // Initialize
     $.getJSON(SWIMMER_DATA_FILE, raw => {
+        BG_Update(raw.laps)
         $('#name').text(raw.name);
         set_stat($('#laps'), raw.laps);
         set_stat($('#meters'), raw.laps * LAP_LENGTH);
@@ -21,8 +36,10 @@ $(document).ready(() => {
     });
     setInterval(() => {
         $.getJSON(SWIMMER_DATA_FILE, raw => {
+            BG_Update(raw.laps)
             set_stat($('#laps'), raw.laps);
             set_stat($('#meters'), raw.laps * LAP_LENGTH);
+
         });
     }, 2000);
 });
